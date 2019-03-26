628d4f17986bf41a9a92357288fa9b6dfa826a82
model <1453043024@qq.com>
2019-03-26 11:50:19 +0800

    model

---
+++ src/main/java/com/lcvc/ebuy_test/web/Test.java
@@ -0,0 +1,14 @@
+package com.lcvc.ebuy_test.web;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String a="admin";
+		String b="admin";
+		System.out.println(a==b);
+	}
+
+}
